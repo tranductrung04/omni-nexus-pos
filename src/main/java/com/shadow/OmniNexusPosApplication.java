adce01c88060ac60ddf35cd6504b82180ebc265f
Shadow23434 <tranductrung370@gmail.com>
2026-06-01 11:53:24 +0700

    initial commit

---
+++ src/main/java/com/shadow/OmniNexusPosApplication.java
@@ -0,0 +1,13 @@
+package com.shadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniNexusPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniNexusPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadow/omninexus/OmniNexusPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.omninexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniNexusPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
